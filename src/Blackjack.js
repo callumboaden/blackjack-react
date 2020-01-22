bead1b1b34d3241f19dbd39049408105a5f06333
callumboaden <boaden.callum@gmail.com>
2020-01-22 18:37:17 +0700

    added Deck class

--- src/Blackjack.js
+++ src/Blackjack.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
+import Deck from './Deck';
 
 class Blackjack extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            deck: [],
+            deck: new Deck(),
             playerHands: [],
             playerBank: 1000,
             dealerHands: []
         }
     }
     render() { 
+        console.log(this.state)
         return ( 
             <div>
                 <h1>Blackjack</h1>
