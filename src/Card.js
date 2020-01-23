134345dc2b2a6fcc271836885039948030d7a92c
callumboaden <boaden.callum@gmail.com>
2020-01-23 18:24:50 +0700

    added svg cards

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,35 @@
-import React from 'react'
-import './Card.css';
+import React, {Component} from 'react';
+import images from "./images";
+
+import "./Card.css";
+const reqSvgs = require.context ('./svg', true, /\.svg$/ )
+
+const svgs = reqSvgs
+  .keys ()
+  .reduce ( ( images, path ) => {
+    images[path] = reqSvgs ( path )
+    return images
+  }, {} );
+
+const image = svgs['./9S.svg'];
+
+console.log(image)
 
 function Card(props) {
-    const { suit, value } = props;
-    return (
-        <div className="Card">
-            <p>{value} of {suit}</p>
-        </div>
-    )
+  const { suit, value, string } = props;
+  const image = svgs[`./${string}.svg`];
+   
+  return (
+    <div className="Card">
+      <div className="Card-image">
+        <img src={image} width="100" />
+      </div>
+
+      <p>
+        {value} of {suit}
+      </p>
+    </div>
+  );
 }
 
 export default Card;
